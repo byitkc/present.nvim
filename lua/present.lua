63b2d53e08e03b9d4b54302eb4b3f4cfd2b34db0
Brandon Young <brandon@byitkc.com>
2026-01-14 08:58:28 -0600

    basic working example

---
+++ lua/present.lua
@@ -0,0 +1,102 @@
+local M = {}
+
+M.setup = function()
+  -- nothing
+end
+
+--- Creates a floating window
+---@fields opts any: The options for the floating window
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+  local footer = opts.footer or ""
+
+  -- Calculate the position to the center of the screen
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+
+  -- Create a buffer
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  -- Define window configuration
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = "minimal",
+    border = "rounded",
+    footer = footer,
+  }
+
+  -- Create the floating window
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+---@class present.Slides
+---@fields slides string[]: The slides of the file
+
+--- Takes some lines and parses them
+---@param lines string[]: The lines in the buffer
+---@return present.Slides:
+local parse_slides = function(lines)
+  local slides = { slides = {} }
+  local current_slide = {}
+
+  local separator = "^#"
+
+  for _, line in ipairs(lines) do
+    if line:find(separator) then
+      if #current_slide > 0 then
+        table.insert(slides.slides, current_slide)
+      end
+
+      current_slide = {}
+    end
+
+    table.insert(current_slide, line)
+  end
+  table.insert(slides.slides, current_slide)
+
+  return slides
+end
+
+M.start_presentation = function(opts)
+  opts = opts or {}
+  opts.bufnr = opts.bufnr or 0
+
+  local lines = vim.api.nvim_buf_get_lines(opts.buf, 0, -1, false)
+  local parsed = parse_slides(lines)
+  local float = create_floating_window()
+  local current_slide = 1
+
+  vim.keymap.set("n", "n", function()
+    current_slide = math.min(current_slide + 1, #parsed.slides)
+    vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+  end, {
+    buffer = float.buf
+  })
+
+  vim.keymap.set("n", "p", function()
+    current_slide = math.min(current_slide - 1, 1)
+    vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[current_slide])
+  end, {
+    buffer = float.buf
+  })
+
+  vim.api.nvim_buf_set_lines(float.buf, 0, -1, false, parsed.slides[1])
+end
+
+-- vim.print(parse_slides({
+--   "# Hello",
+--   "This is something else",
+--   "# World",
+--   "this is another thing",
+-- }))
+M.start_presentation({ buf = 29 })
+
+return M
